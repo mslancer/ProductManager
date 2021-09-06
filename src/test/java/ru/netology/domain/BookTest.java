4db0663303ef07efa7e02d31d1fb33782190590e
mslancer <wolfnw@yandex.ru>
2021-09-06 23:50:26 +0500

    fix

--- src/test/java/ru/netology/domain/BookTest.java
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -21,12 +21,6 @@ class BookTest {
   }
 
   @Test
-  public void shouldNotCastToDifferentClass() {
-    Product product = new Book();
-    TShirt shirt = (TShirt) product;
-  }
-
-  @Test
   public void shouldUseOverridedMethod() {
     Product product = new Book();
     // Вопрос к аудитории: чей метод вызовется?
